854d83c3aa806222540afd62943416dbef6cad53
Phi Pham <phi.phuongpham@auspost.com.au>
2022-02-24 13:55:59 +1100

    Enhance logic to create subscription

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -3,16 +3,23 @@ set -e
 
 bootstrap() {
 	while ! nc -z localhost ${PORT:-8538}; do sleep 0.5; done
-	for topic in $TOPICS;do
+	# TOPICS="topic1 topic2"
+	# SUB_NAMES="sub1=url1,sub11 sub2"
+	subs=($SUB_NAMES)
+	i=0
+	for topic in $TOPICS; do
 		echo "Creating topic $topic"
 		curl -fsSLX PUT http://localhost:${PORT:-8538}/v1/projects/${PROJECT_ID:-project-id}/topics/$topic
-		for sub in $SUB_NAMES; do
+		
+		IFS=','
+		read -a topicSubs <<< "${subs[i]}"
+		for sub in ${topicSubs[@]}; do
 			PUSH_ENDPOINT=${sub##*=}
 			sub=${sub%%=*}
 			[[ "$PUSH_ENDPOINT" == "$sub" ]] && PUSH_ENDPOINT=
 			
 			curl -fsSLX PUT \
-			http://localhost:${PORT:-8538}/v1/projects/${PROJECT_ID:-project-id}/subscriptions/${sub/TOPIC/$topic} \
+			http://localhost:${PORT:-8538}/v1/projects/${PROJECT_ID:-project-id}/subscriptions/${sub} \
 			-H 'Content-Type: application/json' \
 			--data-binary @- <<-JSON
 				{
@@ -24,6 +31,7 @@ bootstrap() {
 				}
 			JSON
 		done
+		i=$i+1
 	done
 }
 
